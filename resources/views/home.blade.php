8a442023e82a02f607a514814ee1bad2bee41a48
dieselgank <adamraya11@gmail.com>
2024-07-16 03:33:25 +0700

    struktur folder

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>Halaman Home</h1>
+</body>
+</html>
